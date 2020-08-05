df1725f97731d7b1e3919fd96a965a079733db9f
jeromrichard <jeromerichard72@yahoo.com.com>
2020-08-05 10:44:15 +0200

    correction account

--- src/pages/account.js
+++ src/pages/account.js
@@ -3,26 +3,26 @@ import { Router } from "@reach/router"
 import { login, logout, isAuthenticated, getProfile } from "../utils/auth"
 import { Link } from "gatsby"
 
-const Home = () => <p>Home</p>
-const MyAccount = () => <p>My Account</p>
+const Home = ({ user }) => {
+  return <p>Hi, {user.name ? user.name : "friend"}!</p>
+}
 const Settings = () => <p>Settings</p>
 const Billing = () => <p>Billing</p>
 
 const Account = () => {
   if (!isAuthenticated()) {
     login()
-  //  return <p>Redirecting to login...</p>
+    return <p>Redirecting to login...</p>
   }
-  
+
   const user = getProfile()
 
   return (
     <>
       <nav>
-        <Link to="/">Home</Link>{" "}
-        <Link to="/account">My Account</Link>{" "}
+        <Link to="/account">Home</Link>{" "}
         <Link to="/account/settings">Settings</Link>{" "}
-        <Link to="/Menu">Collaborateur</Link>{" "}
+        <Link to="/Menu">Collaborateurs</Link>{" "}
         <a
           href="#logout"
           onClick={e => {
@@ -33,10 +33,8 @@ const Account = () => {
           Log Out
         </a>
       </nav>
-      <pre>{JSON.stringify(user, null, 2)}</pre>
       <Router>
-        <Home path="/" />
-        <MyAccount path="/account" />
+        <Home path="/account" user={user} />
         <Settings path="/account/settings" />
         <Billing path="/Menu" />
       </Router>
